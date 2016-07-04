befaa3e3340dbd8ef72fe9ded7502e9370475f46
scoobycoder <scoobycoder@gmail.com>
2016-07-04 17:14:37 -0400

    now counting all words

--- count_spec.rb
+++ count_spec.rb
@@ -8,11 +8,16 @@ describe 'Count Words' do
   end
 
   it 'should return a count one 1 for "President"' do
-    expect(@counter.count('President')).to eq({:President => 1})
+    expect(@counter.count('President')).to eq({1 => 'President'})
   end
 
   it 'should count 11 for the word "that"' do
-    expect(@counter.count('that')).to eq({:that => 11})
+    expect(@counter.count('that')).to eq({11 => 'that'})
+  end
+
+  it 'should return word with highest number of instances' do
+    all_word_counts = @counter.count_all_words.sort.to_h
+    expect(all_word_counts[11]).to eq('our')
   end
 
 end
--- counter.rb
+++ counter.rb
@@ -8,11 +8,18 @@ class Counter
     count_word(word)
   end
 
+  def count_all_words
+    all_word_counts = {}
+
+    @file_contents.each {|word| all_word_counts.merge! count_word(word) }
+    all_word_counts
+  end
+
   private
   def count_word(word)
     word_hash = {}
     words = @file_contents.select { |w| w == word }
-    word_hash.merge! ({word.to_sym => words.count})
+    word_hash.merge! ({words.count => word})
   end
 
   def file_contents(file_name)
